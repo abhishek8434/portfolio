3681b4c853301132ad63c3f6faba37b837a7ca45
Kumar Abhishek <61007819+abhishek8434@users.noreply.github.com>
2024-12-24 20:48:30 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-
 /*!
     * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
-    (function ($) {
+(($ => {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
@@ -40,5 +39,4 @@
     $("body").scrollspy({
         target: "#sideNav",
     });
-})(jQuery); // End of use strict
-
+})(jQuery)); // End of use strict
